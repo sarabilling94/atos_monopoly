608bd8db00b6b752e19651d5c9d707eedfd14f7b
Sara Billing <sara.billing@siemens.com>
2021-11-16 11:19:00 +0100

    figur,spieler

---
+++ Monopoly_Commandline/Monopoly_Commandline/Figur.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monopoly_Commandline
+{
+    class Figur
+    {
+        private string Bezeichnung;
+
+        public Figur(string bezeichnung)
+        {
+            this.Bezeichnung = bezeichnung;
+        }
+    }
+}
---
+++ Monopoly_Commandline/Monopoly_Commandline/Spieler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monopoly_Commandline
+{
+    class Spieler
+    {
+        private Figur Figur;
+        private int Kontostand;
+
+        private int Position;
+
+        public Spieler(Figur figur, int kontostand, int position)
+        {
+            this.Figur = figur;
+            this.Kontostand = kontostand;
+            this.Position = position;
+        }  
+    }
+}
