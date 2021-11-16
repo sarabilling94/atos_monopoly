c2227b448f9aea5ea80df8638fa01fe555297d45
Sara Billing <sara.billing@siemens.com>
2021-11-16 11:09:19 +0100

    würfel

---
+++ Monopoly_Commandline/Monopoly_Commandline/Würfel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monopoly_Commandline
+{
+    class Würfel
+    {
+        static int Würfeln()
+        {
+            Random random = new Random();
+            return random.Next(1, 6);
+        }
+    }
+}
