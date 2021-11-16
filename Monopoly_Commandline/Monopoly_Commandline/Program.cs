d6eebd792d5a8ee3b801f1c5731edf24f7addd5d
Sara Billing <sara.billing@siemens.com>
2021-11-16 10:59:19 +0100

    added project

---
+++ Monopoly_Commandline/Monopoly_Commandline/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Monopoly_Commandline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
