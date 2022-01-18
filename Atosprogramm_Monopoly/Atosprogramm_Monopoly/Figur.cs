54ece63f4bc91d319f4574624f0fb487fd970494
Sara Billing <sara.billing@siemens.com>
2022-01-18 09:10:23 +0100

    copied files from commandline app

---
+++ Atosprogramm_Monopoly/Atosprogramm_Monopoly/Figur.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atosprogramm_Monopoly
+{
+    public class Figur
+    {
+        public string Bezeichnung { get; set; }
+
+        public Figur(string bezeichnung)
+        {
+            this.Bezeichnung = bezeichnung;
+        }
+
+    }
+}
